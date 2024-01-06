6bb85389e200abafe9d0e4df9207d706818c16a5
Dmytro <dmytro.kaminnyk@gmail.com>
2024-01-06 21:11:02 +0200

    Added currency list

--- app/src/main/java/com/mycompany/currencytracker/presentation/currency_list/CurrencyListScreen.kt
+++ app/src/main/java/com/mycompany/currencytracker/presentation/currency_list/CurrencyListScreen.kt
@@ -1,35 +1,47 @@
 package com.mycompany.currencytracker.presentation.currency_list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.mycompany.currencytracker.domain.model.currency.Currency
 import com.mycompany.currencytracker.presentation.currency_list.components.CurrencyListItem
+import com.mycompany.currencytracker.presentation.ui.theme.darkBackgroundColor
 
 @Composable
 fun CurrencyListScreen(
     viewModel: CurrencyListViewModel = hiltViewModel()
-){
+) {
     val state = viewModel.state.value
-    Box(modifier = Modifier.fillMaxSize()){
-        LazyColumn(modifier = Modifier.fillMaxSize()){
-            items(state.currencies) {currency ->
-                CurrencyListItem(currency = currency)
+    val list: MutableList<Currency> = state.currencies.toMutableList()
+    list.removeIf { it.symbol == "BTC"}
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(darkBackgroundColor)
+    ) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            itemsIndexed(list) { curr_number, currency ->
+                CurrencyListItem(currency = currency, curr_number = curr_number + 1)
             }
         }
-        if(state.error.isNotBlank()) {
+        if (state.error.isNotBlank()) {
             Text(
                 text = state.error
             )
         }
-        if(state.isLoading){
+        if (state.isLoading) {
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
     }
--- app/src/main/java/com/mycompany/currencytracker/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/mycompany/currencytracker/presentation/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val mainTextColor = Color(0xFFFFFFFF)
+val darkBackgroundColor = Color(0xFF212121)
+
+val rateUpColor = Color(0xFF16c784)
+val rateDownColor = Color(0xFFea3943)
+
+val secondTextColor = Color(0xFF999999)
