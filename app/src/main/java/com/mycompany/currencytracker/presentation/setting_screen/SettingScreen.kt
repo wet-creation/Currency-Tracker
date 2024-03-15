56fe27d5d24929fc4ab8f95bce36abff3a58eb89
wet_creation <mnedobitkin@gmail.com>
2024-03-15 17:24:16 +0200

    refactor v 0.6

--- app/src/main/java/com/mycompany/currencytracker/presentation/setting_screen/SettingScreen.kt
+++ app/src/main/java/com/mycompany/currencytracker/presentation/setting_screen/SettingScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
